57c7e97e232e2e5eabf97a0a201f1eee508045df
John Newman <newmanj903@gmail.com>
2018-04-23 14:08:54 +0100

    first test for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+
+
+  describe '#balance' do
+    it 'should have a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
