cd8617fc2623d75a2a1a096277ec9f77aa054ba8
Possed <r0xana.pirvu@yahoo.com>
2018-04-23 14:44:09 +0100

    defined top_up method and passed the test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
-
-
   describe '#balance' do
     it 'should have a default balance of 0' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'should update balance to 10' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by 10
     end
   end
 end
